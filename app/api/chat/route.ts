45ac17ea944fd69ba03111959845e3301058cde3
Artem Murzo <murzotom@rocketmail.com>
2026-01-30 11:00:22 -0500

    chat component draft; fix for tailwind-animate-css error in globals.css

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,19 +1,28 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText, UIMessage, convertToModelMessages } from "ai";
 
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
 export async function POST(req: Request) {
     try {
-        const { messages }: { messages: UIMessage[] } = await req.json();
+        const { messages, webSearch, }: { messages: UIMessage[]; webSearch: boolean; } = await req.json();
 
         const model = openai('gpt-4.1-mini');
 
         const modelMessages = await convertToModelMessages(messages);
         const result = streamText({
             model,
-            messages: modelMessages
+            messages: modelMessages,
+            system:
+                'You are a helpful assistant that can answer questions and help with tasks',
         });
 
-        return result.toUIMessageStreamResponse();
+        // send sources and reasoning back to the client
+        return result.toUIMessageStreamResponse({
+            sendSources: true,
+            sendReasoning: true,
+        });
     } catch (error) {
         console.error(error);
         return new Response('Internal Server Error', { status: 500 });
