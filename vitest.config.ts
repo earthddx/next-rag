e9e50e80176bb5f70c5604a4071433ef92b7ef3d
Artem Murzo <murzotom@rocketmail.com>
2026-02-17 11:32:32 -0500

    jsdom for vitest config and cleanup between tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import path from 'path'
 export default defineConfig({
   test: {
     include: ['**/*.test.ts', '**/*.test.tsx'],
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
   },
   resolve: {
     alias: {
