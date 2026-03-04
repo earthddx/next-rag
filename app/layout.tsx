309a883ffa6430297907dc77340bd7fc919f5773
Artem Murzo <murzotom@rocketmail.com>
2026-03-04 18:00:48 -0500

    updated faviccon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ChatDocs",
   description: "Chat with your documents",
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
